24bc5e0816564e5650bd8eaceaae1b200457b73e
khcho96 <khcho1492@gmail.com>
2025-10-15 19:44:37 +0900

    rename(Input): 클래스 이름 InputObj->Input 으로 수정

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -2,7 +2,7 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class InputObj {
+public class Input {
 
     public String readInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
--- src/main/java/calculator/Output.java
+++ src/main/java/calculator/Output.java
@@ -2,7 +2,7 @@ package calculator;
 
 import java.math.BigInteger;
 
-public class OutputObj {
+public class Output {
 
     public void printResult(BigInteger result) {
         System.out.println("결과 : " + result);
