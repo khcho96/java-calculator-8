dd3f2e11f408de55f91dd809f9dd219fcfc42184
khcho96 <khcho1492@gmail.com>
2025-10-15 18:46:10 +0900

    refactor(Extractor): extractCustomDelimiter 메서드 수정 1 -> "//".length() 으로 유지보수성 향상

--- src/main/java/calculator/Extractor.java
+++ src/main/java/calculator/Extractor.java
@@ -3,7 +3,7 @@ package calculator;
 public class Extractor {
 
     public static String extractCustomDelimiter(String input) {
-        int beginIndex = input.indexOf("//") + 2;
+        int beginIndex = input.indexOf("//") + "//".length();
         int endIndex = input.indexOf("\\n");
         return input.substring(beginIndex, endIndex);
     }
