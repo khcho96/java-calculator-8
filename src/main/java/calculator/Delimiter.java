45e426ab7fafaae3cbcde2cdfcbc0331715b940b
khcho96 <khcho1492@gmail.com>
2025-10-15 18:26:59 +0900

    feat(Delimiter): 구분자를 저장하고 처리하는 기능을 가진 Delimiter 클래스 구현 add 메서드 추가 - 커스텀 구분자를 기본 구분자에 추가하는 기능 추가

--- src/main/java/calculator/Delimiter.java
+++ src/main/java/calculator/Delimiter.java
@@ -1,24 +1,14 @@
 package calculator;
 
-import java.util.List;
-import java.util.stream.Stream;
-
 public class Delimiter {
-
-    public static final String DEFAULT_DELIMITER = ",:";
-    private final String delimiter;
+    private static final String DEFAULT_DELIMITER = ",:";
+    private String delimiter;
 
     public Delimiter() {
-        this.delimiter = "[" + DEFAULT_DELIMITER + "]";
+        this.delimiter = DEFAULT_DELIMITER;
     }
 
-    public Delimiter(String customDelimiter) {
-        this.delimiter = "[" + DEFAULT_DELIMITER + customDelimiter + "]";
-    }
-
-    public List<String> split(String target) {
-        return Stream.of(target.split(delimiter)).
-                filter(s->!s.isBlank()).
-                toList();
+    public void add(String customDelimiter) {
+        this.delimiter = DEFAULT_DELIMITER + customDelimiter;
     }
 }
