71fbe0132a2ee9e9346f6daa749903c204157867
khcho96 <khcho1492@gmail.com>
2025-10-15 21:05:17 +0900

    fix(Input): 실패 테스트 케이스를 위한 버그 수정 NsTest의 run 메서드에 빈문자열("")을 전달했을 때 Console.readLine 메서드에서 던지는 NoSuchElementException로 인한 테스트케이스 실패 해당 실패를 해결하기 위한 버그 수정

--- src/main/java/calculator/Input.java
+++ src/main/java/calculator/Input.java
@@ -1,11 +1,16 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
 
 public class Input {
 
     public String readInput() {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return Console.readLine();
+        try {
+            return Console.readLine();
+        } catch (NoSuchElementException e) {
+            return "";
+        }
     }
 }
