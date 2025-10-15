ae98e83ddea03f21bd44e56430b1f764c2f1f91e
khcho96 <khcho1492@gmail.com>
2025-10-15 19:33:23 +0900

    feat(Adder): 양수 값들을 더하는 기능을 가진 클래스 및 getSum 메서드 추가 getSum 메서드 - 값들을 모두 더해 결과를 산출하는 기능 추가

---
+++ src/main/java/calculator/Adder.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public class Adder {
+
+    public static BigInteger getSum(List<BigInteger> numbers) {
+        return numbers.stream()
+                .reduce(BigInteger.ZERO, BigInteger::add);
+    }
+}
