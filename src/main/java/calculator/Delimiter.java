bdc0ecab57242581eea2d4d8939e26f81f2cc538
khcho96 <khcho1492@gmail.com>
2025-10-15 18:51:50 +0900

    feat(Delimiter): split, wrap 메서드 추가 split 메서드 - 추출한 문자열을 구분자로 분리한 후 공백만 존재하는 값은 제거하는 기능 wrap 메서드 - 구분자를 대괄호(`[]`)로 감싸주는 메서드

--- src/main/java/calculator/Delimiter.java
+++ src/main/java/calculator/Delimiter.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Delimiter {
     private static final String DEFAULT_DELIMITER = ",:";
     private String delimiter;
@@ -10,5 +13,16 @@ public class Delimiter {
 
     public void add(String customDelimiter) {
         this.delimiter = DEFAULT_DELIMITER + customDelimiter;
+    }
+
+    public List<String> split(String input) {
+        wrap();
+        return Stream.of(input.split(delimiter)).
+                filter(s->!s.isBlank()).
+                toList();
+    }
+
+    private void wrap() {
+        delimiter = "[" + delimiter + "]";
     }
 }
