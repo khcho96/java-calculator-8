ff4976f6d0c69f45b30ee77f9b2ea7228179ed95
khcho96 <khcho1492@gmail.com>
2025-10-15 16:59:46 +0900

    refactor(Validator): static 메서드로 변경

--- src/main/java/calculator/Validator.java
+++ src/main/java/calculator/Validator.java
@@ -2,13 +2,13 @@ package calculator;
 
 public class Validator {
 
-    public void validateNull(String input) {
+    public static void validateNull(String input) {
         if (input == null) {
             throw new IllegalArgumentException("입력이 유효하지 않습니다.");
         }
     }
 
-    public void validateInputFormat(String input) {
+    public static void validateInputFormat(String input) {
         if (!input.strip().matches("^(//.+\\\\n)?.*")) {
             throw new IllegalArgumentException("잘못된 형식의 입력입니다.");
         }
