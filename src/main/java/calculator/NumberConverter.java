7e8b8e429662a64f7b4f3f61d261b858662b4ec4
khcho96 <khcho1492@gmail.com>
2025-10-15 19:29:58 +0900

    feat(NumberConverter): 문자열을 양수로 변환하는 기능을 가진 클래스 및 convertStringToNumber 메서드 추가 분리한 값들의 양쪽 공백을 제거한 후 양수로 변환하는 기능 추가

---
+++ src/main/java/calculator/NumberConverter.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import java.math.BigInteger;
+import java.util.List;
+
+public class NumberConverter {
+
+    public static List<BigInteger> convertStringToNumber(List<String> strings) {
+        return strings.stream()
+                .map(BigInteger::new)
+                .toList();
+    }
+}
