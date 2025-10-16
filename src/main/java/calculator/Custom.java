daff715657ec18c3cbd4af7cf13ba166b6ab2018
khcho96 <khcho1492@gmail.com>
2025-10-16 14:04:21 +0900

    refactor(Custom): 입력값 형식에 대한 판단을 위해 필요한 정규식 패턴을 상수로 저장

--- src/main/java/calculator/Custom.java
+++ src/main/java/calculator/Custom.java
@@ -1,8 +1,13 @@
 package calculator;
 
 public class Custom {
+    public static final String IS_CUSTOM = "^//.+?\\\\n.*";
+    public static final String CUSTOM_FORMAT_PREFIX = "//";
+    public static final String CUSTOM_FORMAT_SUFFIX = "\\n";
+    public static final String VALID_INPUT_FORMAT = "^(//.+?\\\\n)?.*";
+    public static final String NUMBER_FORMAT = "\\d+";
 
     public static boolean isCustom(String input) {
-        return input.strip().matches("^//.+?\\\\n.*");
+        return input.strip().matches(IS_CUSTOM);
     }
 }
