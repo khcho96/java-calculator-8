dd0f87728187f218b784d1931df7c6d3a1e63d3f
khcho96 <khcho1492@gmail.com>
2025-10-16 13:38:49 +0900

    feat(ErrorMessage): 에러메시지를 상수로 저장하는 클래스 생성

---
+++ src/main/java/calculator/ErrorMessage.java
@@ -0,0 +1,6 @@
+package calculator;
+
+public class ErrorMessage {
+    public static final String INVALID_INPUT_ERROR = "입력이 유효하지 않습니다.";
+    public static final String INPUT_FORMAT_ERROR = "잘못된 형식의 입력입니다.";
+}
