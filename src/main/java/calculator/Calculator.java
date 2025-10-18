52f575cab3565a8a98a5a88e357af76b95701578
khcho96 <khcho1492@gmail.com>
2025-10-18 20:48:57 +0900

    refactor(Calculator): calculate 메서드 수정 책임을 분산시킴으로써 최소한의 메인 로직만 다루도록 수정

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,47 +1,20 @@
 package calculator;
 
-import static calculator.util.Adder.getSum;
-import static calculator.util.Extractor.extractCustomDelimiter;
-import static calculator.util.Extractor.extractTarget;
-import static calculator.data.Format.isCustom;
-import static calculator.util.NumberConverter.convertStringToNumber;
-import static calculator.util.Validator.validateInputFormat;
-import static calculator.util.Validator.validateNull;
-import static calculator.util.Validator.validateNumber;
-
-import calculator.data.Delimiter;
+import calculator.data.Numbers;
+import calculator.data.RawInputValue;
+import calculator.data.Target;
 import java.math.BigInteger;
-import java.util.List;
 
 public class Calculator {
 
     public BigInteger calculate(String input) {
-        // 입력값을 검증한다.
-        validateNull(input);
-        validateInputFormat(input);
-
-        // 구분자 객체 생성
-        Delimiter delimiter = new Delimiter();
-
-        String target = input;
-        if (isCustom(input)) {
-            // 커스텀 구분자가 지정되었다면 커스텀 구분자를 추출 및 기본 구분자에 추가한다.
-            String customDelimiter = extractCustomDelimiter(input);
-            delimiter.add(customDelimiter);
-            // 타겟 문자열을 추출한다.
-            target = extractTarget(input);
-        }
+        // 입력 문자열이 커스텀 문자열이면 타겟 문자열을 추출한다.
+        Target target = new RawInputValue(input).getTargetIfCustom();
 
         // 타겟 문자열을 구분자로 분리한다.
-        List<String> splitValues = delimiter.split(target);
-
-        // 분리한 값들을 검증한다.
-        validateNumber(splitValues);
-
-        // 검증한 값들을 양수로 변환한다.
-        List<BigInteger> numbers = convertStringToNumber(splitValues);
+        Numbers numbers = target.split();
 
         // 양수 값들을 모두 더해 결과를 산출한다.
-        return getSum(numbers);
+        return numbers.calculateSum();
     }
 }
