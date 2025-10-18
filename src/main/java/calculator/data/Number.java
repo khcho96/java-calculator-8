fb9df34cd2c2d70ff5fe1a36ce1cea116c20d845
khcho96 <khcho1492@gmail.com>
2025-10-18 20:35:29 +0900

    feat(Number): Number 클래스 생성 양수 형태의 문자열인지 검증 후 양수로 변환한 값을 저장하는 클래스 값에 대한 검증 또한 각 객체의 몫으르 수정 -> Validator 클래스의 역할을 이제 각 Number 객체가 수행

---
+++ src/main/java/calculator/data/Number.java
@@ -0,0 +1,26 @@
+package calculator.data;
+
+import static calculator.data.Format.isNotValidNumberFormat;
+import static calculator.error.ErrorMessage.INPUT_FORMAT_ERROR;
+import static calculator.util.NumberConverter.convertStringToNumber;
+
+import java.math.BigInteger;
+
+public class Number {
+    private final BigInteger number;
+
+    public Number(String value) {
+        validateValue(value);
+        this.number = convertStringToNumber(value);
+    }
+
+    public BigInteger getNumber() {
+        return number;
+    }
+
+    public static void validateValue(String value) {
+        if (isNotValidNumberFormat(value)) {
+            throw new IllegalArgumentException(INPUT_FORMAT_ERROR);
+        }
+    }
+}
