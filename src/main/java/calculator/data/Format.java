fc275ebef652873ff9f2fd3053e1f4b7116e991a
khcho96 <khcho1492@gmail.com>
2025-10-18 15:21:56 +0900

    refactor(Format): 문자열에서 패턴의 위치를 찾는 기능을 메서드 내부에서 해결하도록 수정 Extractor에서 직접 Format의 상수를 사용하면서 문자열에서 패턴의 위치를 찾는 기존의 구현에서 Format 클래스 자체 내부에서 해당 기능을 메서드로 구현. Extractor는 만들어진 메서드를 호출만 하면 된다는 이점과 Format의 상수를 클래스 내부에서만 사용하면서 외부에 노출 없이 보호할 수 있다는 이점이 생김. 구현한 메서드: getCustomDelimiterBeginIndex, getCustomDelimiterEndIndex, getTargetBeginIndex 상수는 public -> private로 접근제어자 변경: IS_CUSTOM, CUSTOM_FORMAT_PREFIX, CUSTOM_FORMAT_SUFFIX

--- src/main/java/calculator/data/Format.java
+++ src/main/java/calculator/data/Format.java
@@ -1,13 +1,25 @@
 package calculator.data;
 
 public class Format {
-    public static final String IS_CUSTOM = "^//.+?\\\\n.*";
-    public static final String CUSTOM_FORMAT_PREFIX = "//";
-    public static final String CUSTOM_FORMAT_SUFFIX = "\\n";
+    private static final String IS_CUSTOM = "^//.+?\\\\n.*";
+    private static final String CUSTOM_FORMAT_PREFIX = "//";
+    private static final String CUSTOM_FORMAT_SUFFIX = "\\n";
     public static final String VALID_INPUT_FORMAT = "^(//.+?\\\\n)?.*";
     public static final String NUMBER_FORMAT = "\\d+";
 
     public static boolean isCustom(String input) {
         return input.strip().matches(IS_CUSTOM);
+    }
+
+    public static int getCustomDelimiterBeginIndex(String input) {
+        return input.indexOf(CUSTOM_FORMAT_PREFIX) + CUSTOM_FORMAT_PREFIX.length();
+    }
+
+    public static int getCustomDelimiterEndIndex(String input) {
+        return input.indexOf(CUSTOM_FORMAT_SUFFIX);
+    }
+
+    public static int getTargetBeginIndex(String input) {
+        return input.indexOf(CUSTOM_FORMAT_SUFFIX) + CUSTOM_FORMAT_SUFFIX.length();
     }
 }
