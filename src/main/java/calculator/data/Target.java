c833727bbdbfe2abc9218131f6eb3751e50b0094
khcho96 <khcho1492@gmail.com>
2025-10-18 20:43:52 +0900

    feat(Target): Target 클래스 생성 - 역할 1. 타겟 문자열을 저장 2. Delimiter 객체에게 타겟 문자열을 분리하는 작업을 요청

---
+++ src/main/java/calculator/data/Target.java
@@ -0,0 +1,13 @@
+package calculator.data;
+
+public class Target {
+    private final String target;
+
+    public Target(String target) {
+        this.target = target;
+    }
+
+    public Numbers split() {
+        return Delimiter.getInstance().split(target);
+    }
+}
